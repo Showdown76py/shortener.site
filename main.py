ed0a13997e8230666394580d013dbc75077bb211
Showdown76py <66854264+Showdown76py@users.noreply.github.com>
2021-11-03 15:02:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ def _main_page():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=os.environ.get('PORT'), debug=True, threaded=True)
